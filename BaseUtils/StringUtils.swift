920c4eb6fee5ab8afbe3ade0ce67a1209715956d
Etienne Goulet-Lang <egouletlang@Etiennes-MacBook-Pro.local>
2016-12-02 23:38:22 -0800

    "podspec"

---
+++ BaseUtils/StringUtils.swift
@@ -0,0 +1,15 @@
+//
+//  StringUtils.swift
+//  BaseUtils
+//
+//  Created by Etienne Goulet-Lang on 12/2/16.
+//  Copyright © 2016 Etienne Goulet-Lang. All rights reserved.
+//
+
+import Foundation
+
+class StringUtils {
+    
+    
+    
+}
